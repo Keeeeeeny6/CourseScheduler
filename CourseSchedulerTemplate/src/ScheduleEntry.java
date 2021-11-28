e5014f072b28adaa7200ff6fd24c690fa7376cae
Keeeeeeny6 <69370105+Keeeeeeny6@users.noreply.github.com>
2021-11-27 21:13:13 -0500

    Add files via upload

---
+++ CourseSchedulerTemplate/src/ScheduleEntry.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kenny
+ */
+public class ScheduleEntry {
+    private String semesters;
+    private String studentId;
+    private String courseCode;
+    private String status;
+}
+
+
